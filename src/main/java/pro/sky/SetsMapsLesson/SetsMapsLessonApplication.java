94076e7c82fd8c27a20d79b8bc42877e62d5cbff
student <i.i.f@mail.ru>
2022-05-14 18:46:43 +0800

    Initial commit

---
+++ src/main/java/pro/sky/SetsMapsLesson/SetsMapsLessonApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.SetsMapsLesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetsMapsLessonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SetsMapsLessonApplication.class, args);
+	}
+
+}
