2f93cb120d6f66ede337ca580c985fbb42d2005c
student <i.i.f@mail.ru>
2022-05-14 20:33:40 +0800

    первая часть

---
+++ src/main/java/pro/sky/SetsMapsLesson/controller/LettersController.java
@@ -0,0 +1,29 @@
+package pro.sky.SetsMapsLesson.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pro.sky.SetsMapsLesson.service.LettersService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/check/letters")
+public class LettersController {
+
+    private final LettersService lettersService;
+
+    public LettersController (LettersService lettersService) {
+        this.lettersService = lettersService;
+    }
+
+    @GetMapping("/uniq")
+    public List<String> uniq () {
+        return lettersService.uniq();
+    }
+
+    @GetMapping("/repeatcount")
+    public Integer countOfRepeat () {
+        return lettersService.countOfRepeat();
+    }
+}
